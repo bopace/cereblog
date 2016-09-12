b8890c930239eb2fa395d778fd541953b35796a6
Bo Pace <bopace@gmail.com>
2016-09-12 12:25:48 -0600

    Fixing some authentication functionality, adding gitignore

--- app/config/passport.js
+++ app/config/passport.js
@@ -6,13 +6,13 @@ module.exports = function (passport) {
     passport.serializeUser(function (user, done) {
         done(null, user.id);
     });
-    
+
     passport.deserializeUser(function (id, done) {
         User.findById(id, function (err, user) {
             done(err, user);
         });
     });
-    
+
     passport.use(new GitHubStrategy({
 		clientID: configAuth.githubAuth.clientID,
 		clientSecret: configAuth.githubAuth.clientSecret,
